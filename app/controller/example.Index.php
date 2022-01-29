8542318feb9f3a7e0430caf15cfe0e9aabc3693f
AppleIdLoginHelper <98651378+AppleIdLoginHelper@users.noreply.github.com>
2022-01-29 22:19:17 +0800

    Initialize the project

---
+++ app/controller/example.Index.php
@@ -0,0 +1,116 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+use think\facade\View;
+use app\model\MailList;
+use app\model\MailContent;
+
+class Index extends BaseController
+{
+    function getClient()
+    {
+        $path = dirname(dirname(dirname(__FILE__)));
+        
+        $client = new \Google_Client();
+        $client->setApplicationName('Gmail API PHP Quickstart');
+        $client->setScopes(\Google_Service_Gmail::GMAIL_READONLY);
+        $client->setAuthConfig($path . '/credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        $tokenPath = $path . '/token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        if ($client->isAccessTokenExpired()) {
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            }
+        }
+
+        return $client;
+    }
+
+    public function test()
+    {
+        // 开了 oauth2.0 的 gmail 账户 也是接收其他 gmail 转发邮件的收件箱
+        $email_address = '';
+
+        // '你买的有google voice号的gmail账户' => '(对应下载的软件名称或其他备注) 绑定那个gv号的apple id账户',
+        
+        $email_correspondence = [
+            '' => '',
+        ];
+        
+        $client = self::getClient();
+        $httpClient = $client->authorize();
+
+        // https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list
+
+        $list = $httpClient->get('https://gmail.googleapis.com/gmail/v1/users/'.$email_address.'/messages?q=Apple ID');
+        $response = $list->getBody();
+        $object = json_decode($response, true);
+        
+        foreach ($object['messages'] as $data)
+        {
+            $id = $data['id'];
+            if (MailList::where('thread_id', $id)->find() == null) {
+                $record = new MailList;
+                $record->thread_id = $id;
+                $record->created_at = time();
+                $record->save();
+            }
+
+            if (MailContent::where('thread_id', $id)->find() == null) {
+                // https://developers.google.com/gmail/api/reference/rest/v1/users.messages
+                
+                // get
+                $body = $httpClient->get('https://gmail.googleapis.com/gmail/v1/users/'.$email_address.'/messages/'.$id);
+                $response = $body->getBody();
+                $object = json_decode($response, true);
+
+                // process data
+
+                foreach ($object['payload']['headers'] as $item)
+                {
+                    // 收信时间
+                    if ($item['name'] == 'Date') {
+                        $receive_time = $item['value'];
+                        $receive_time = date('Y-m-d H:i:s', strtotime($receive_time));
+                    }
+
+                    // 最终收信人
+                    if ($item['name'] == 'X-Forwarded-To') {
+                        $final_recipient = $item['value'];
+                    }
+
+                    // 原始收信人
+                    if ($item['name'] == 'Delivered-To') {
+                        $original_recipient = $item['value'];
+                    }
+                }
+                
+                // save
+                $record = new MailContent;
+                $record->thread_id = $id;
+                $record->receive_time = $receive_time;
+                $record->final_recipient = $final_recipient ?? $email_address;
+                $record->original_recipient = $original_recipient;
+                $record->correspondence = $email_correspondence[$original_recipient];
+                $record->snippet = substr($object['snippet'], 34, 6);
+                $record->created_at = time();
+                $record->save();
+            }
+        }
+
+        $msg = MailContent::limit(10)
+        ->order('created_at', 'desc')
+        ->select();
+        
+        View::assign('msg', $msg);
+        return View::fetch('../app/view/index.html');
+    }
+}
---
+++ quickstart.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// [START gmail_quickstart]
+require __DIR__ . '/vendor/autoload.php';
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Gmail API PHP Quickstart');
+    $client->setScopes(Google_Service_Gmail::GMAIL_READONLY);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+// 获取 API 客户端并构造服务对象。
+$client = getClient();
+$service = new Google_Service_Gmail($client);
+
+// 打印用户帐户中的标签。
+$user = 'me';
+$results = $service->users_labels->listUsersLabels($user);
+
+if (count($results->getLabels()) == 0) {
+  print "No labels found.\n";
+} else {
+  print "Labels:\n";
+  foreach ($results->getLabels() as $label) {
+    printf("- %s\n", $label->getName());
+  }
+}
